8c984de40869e4b9e3a910de37a35464a7ead487
Rodrigo Lazoti <rodrigolazoti@yahoo.com.br>
2010-01-07 09:33:54 -0200

    first commit

---
+++ view_stocks.rb
@@ -0,0 +1,16 @@
+require "rubygems"
+require "yahoofinance"
+
+#PETR4.SA => Petrobras / Bovespa
+#VALE5.SA => Vale / Bovespa
+#AAPL => Apple Inc. / Nasdaq
+#JAVA => Sun Microsystems / Nasdaq
+
+quote_symbols = "petr4.sa,vale5.sa,aapl,java"
+quote_type = YahooFinance::RealTimeQuote
+
+YahooFinance::get_quotes( quote_type, quote_symbols ) do |quote|	
+	puts "STOCK: #{quote.name} (#{quote.symbol})"
+	puts "PRICE: #{quote.ask} (#{quote.change})"
+	puts "-" * 50
+end
